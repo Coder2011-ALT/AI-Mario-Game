760500032f1026a6a2ae20f060b1298d3569788c
Gyanesh Satyawali <70893994+Coder2011-ALT@users.noreply.github.com>
2021-11-23 16:25:19 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -297,13 +297,13 @@ function autoControl(character) {
 /* Manual control character */
 function manualControl(character) {
   if (character.live) {
-    if (noseX < 300) {
+    if (noseX > 300) {
       character.velocity.x -= gameConfig.moveSpeed;
       character.changeAnimation("move");
       character.mirrorX(-1);
     }
 
-    if (noseX > 300) {
+    if (noseX < 300) {
       character.velocity.x += gameConfig.moveSpeed;
       character.changeAnimation("move");
       character.mirrorX(1);
